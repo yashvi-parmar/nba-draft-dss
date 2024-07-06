814125aa512f970c09ba839fa61e318051c64b43
yashvi-parmar <yparmar@uwaterloo.ca>
2024-07-06 08:41:14 -0400

    changes

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def recommend():
     return render_template('results.html', players=top_3_players.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
